ec044aa1b579920df00c651f690b0e2d5a431dc1
joseverticurl <joseph.ambrose@verticurl.com>
2025-01-02 11:19:03 +0530

    allowing max-cells 5 for herobanner

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,9 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'xwalk/max-cells': ['error', {
+      '*': 4,
+      herobanner: 5,
+    }]
   },
 };
