63019b4441377095f11279ae3888b7fbaf4d5a1b
AvinashRay1702 <avinashkumar.ray@pennywisesolutions.com>
2025-01-24 13:35:18 +0530

    about-us-responsive-design-created

--- scripts/mock-data.js
+++ scripts/mock-data.js
@@ -1,32 +1,93 @@
 export const MOCK_CONTENT = {
-  columns: `
-        <div class="columns-wrapper"><div class="columns block columns-2-cols" data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns" data-aue-type="container" data-aue-model="columns" data-aue-label="Columns" data-aue-filter="columns" data-aue-behavior="component" data-block-name="columns" data-block-status="loaded">
-    <div>
-        <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col1" data-aue-type="container" data-aue-label="Column" data-aue-filter="column">
-        <h2 data-aue-behavior="component" data-aue-model="title" data-aue-label="Title" id="what-is-lorem-ipsum" data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col1/title" data-aue-prop="title" data-aue-type="text">What is Lorem Ipsum?</h2>
-    <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col1/text" data-aue-behavior="component" data-aue-prop="text" data-aue-label="Text" data-aue-filter="text" data-aue-type="richtext"><p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col2" data-aue-type="container" data-aue-label="Column" data-aue-filter="column">
-<h2 data-aue-behavior="component" data-aue-model="title" data-aue-label="Title" id="why-do-we-use-it" data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col2/title" data-aue-prop="title" data-aue-type="text">Why do we use it?</h2>
-    <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row1/col2/text" data-aue-behavior="component" data-aue-prop="text" data-aue-label="Text" data-aue-filter="text" data-aue-type="richtext"><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p></div>
-</div>
-    </div>
-<div>
-        <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col1" data-aue-type="container" data-aue-label="Column" data-aue-filter="column">
-<h2 data-aue-behavior="component" data-aue-model="title" data-aue-label="Title" id="where-does-it-come-from" data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col1/title" data-aue-prop="title" data-aue-type="text">Where does it come from?</h2>
-    <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col1/text" data-aue-behavior="component" data-aue-prop="text" data-aue-label="Text" data-aue-filter="text" data-aue-type="richtext"><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p></div>
-    <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col1/text_0" data-aue-behavior="component" data-aue-prop="text" data-aue-label="Text" data-aue-filter="text" data-aue-type="richtext"><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col2" data-aue-type="container" data-aue-label="Column" data-aue-filter="column">
-<h2 data-aue-behavior="component" data-aue-model="title" data-aue-label="Title" id="where-can-i-get-some" data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col2/title" data-aue-prop="title" data-aue-type="text">Where can I get some?</h2>
-
-    <div data-aue-resource="urn:aemconnection:/content/genting-poc/index/jcr:content/root/section_1/columns/row2/col2/text" data-aue-behavior="component" data-aue-prop="text" data-aue-label="Text" data-aue-filter="text" data-aue-type="richtext"><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p></div>
-</div>
-
-    </div>
-
+ 'hero-banner': `
+  <div data-aue-resource="urn:aemconnection:/content/eds-universaleditor-neworg/index/heroandtilecomponent/jcr:content/root/section/block" data-aue-type="container" data-aue-behavior="component" data-aue-model="hero-banner" data-aue-label="Hero Banner" data-aue-filter="hero-banner" class="hero-banner block" data-block-name="hero-banner" data-block-status="loaded"><div class="hero-banner-container">
+  <picture>
+    <source media="(max-width: 768px)" srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/3818aa4f34615b927264d6d8cab07f1e20d364cf0b7277c747dd56359fc99bce?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540">
+    <source media="(max-width: 1024px)" srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/3818aa4f34615b927264d6d8cab07f1e20d364cf0b7277c747dd56359fc99bce?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540">
+    <source media="(min-width: 1025px)" srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/3818aa4f34615b927264d6d8cab07f1e20d364cf0b7277c747dd56359fc99bce?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540">
+    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/3818aa4f34615b927264d6d8cab07f1e20d364cf0b7277c747dd56359fc99bce?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540" alt="https://delivery-p35060-e135910.adobeaemcloud.com/adobe/assets/urnaem:b3f34f98-e086-4a78-8cb9-9ddb6bc778e9" class="hero-image" loading="eager">
+  </picture>
+ 
+  <h1 class="hero-title">
+    A Better World Through Sustainable Growth
+  </h1>
+<div class="hero-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1502s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+  <span class="icon-wrapper hero-arrow-icon" style="color: white">
+    <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 4L10.59 5.41L16.17 11H4V13H16.17L10.59 18.59L12 20L20 12L12 4Z" fill="currentColor"></path>
+</svg>
+  </span>
 </div></div>
       `,
+  'about-us':`<div class="about-us-wrapper"><div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_677444752" data-aue-type="container" data-aue-behavior="component" data-aue-model="aboutus" data-aue-label="About Us" data-aue-filter="aboutus" class="about-us block" style="" data-block-name="about-us" data-block-status="loaded">
+<div>
+<div><p data-aue-prop="title" data-aue-label="Title" data-aue-type="text">Sustainability Initiatives</p></div>
+</div>
+<div>
+<div><p data-aue-prop="heading" data-aue-label="Heading" data-aue-type="text">Our Commitment to a Sustainable Future</p></div>
+</div>
+<div>
+<div><div data-aue-prop="sub-heading" data-aue-label="Sub Heading" data-aue-filter="text" data-aue-type="richtext"><p>We have played a pivotal role in driving tourism growth, establishing the sector as a key economic engine. Through stakeholder collaborations, we’re fostering mutual growth for communities, investors, the nation,<br>and the planet.</p></div></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_677444752/item_475754375" data-aue-type="component" data-aue-model="linkfield" data-aue-label="Link Field">
+<div><p class="button-container"><a href="/content/genting-singapore/index/contact.html" title="Contact" data-aue-prop="linkText" data-aue-label="Text" data-aue-type="text" class="button">/content/genting-singapore/index/contact</a></p></div>
+<div><p data-aue-prop="linkTarget" data-aue-label="Link Target" data-aue-type="text">_self</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_677444752/item" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div><picture><img src="/content/dam/gens-dev/Icon.png" data-aue-prop="feature-icon" data-aue-label="Feature Icon" data-aue-type="media"></picture></div>
+<div><p data-aue-prop="feature-icon-alt" data-aue-label="Feature Icon Alt Text" data-aue-type="text">bag</p></div>
+<div></div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">Responsible &amp; Ethical Business</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_677444752/1_1737471298550" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div><picture><img src="/content/dam/gens-dev/globe-coin.png" data-aue-prop="feature-icon" data-aue-label="Feature Icon" data-aue-type="media"></picture></div>
+<div><p data-aue-prop="feature-icon-alt" data-aue-label="Feature Icon Alt Text" data-aue-type="text">globe coin</p></div>
+<div></div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">Positive Socio-Economic Impact</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_677444752/2_1737471326743" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div><picture><img src="/content/dam/gens-dev/globe-hand.png" data-aue-prop="feature-icon" data-aue-label="Feature Icon" data-aue-type="media"></picture></div>
+<div><p data-aue-prop="feature-icon-alt" data-aue-label="Feature Icon Alt Text" data-aue-type="text">globe hand</p></div>
+<div></div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">Care for Our Planet</p></div>
+</div>
+ 
+</div></div>`,
+  'about-us':`<div class="about-us-wrapper"><div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353" data-aue-type="container" data-aue-behavior="component" data-aue-model="aboutus" data-aue-label="About Us" data-aue-filter="aboutus" class="about-us block" style="" data-block-name="about-us" data-block-status="loaded">
+<div>
+<div><p data-aue-prop="title" data-aue-label="Title" data-aue-type="text">About Us</p></div>
+</div>
+<div>
+<div><p data-aue-prop="heading" data-aue-label="Heading" data-aue-type="text">An Industry Pioneer</p></div>
+</div>
+<div>
+<div><div data-aue-prop="sub-heading" data-aue-label="Sub Heading" data-aue-filter="text" data-aue-type="richtext"><p>We are uniquely positioned to enable long-term sustainable growth,both locally and globally, through integrated resort developments.</p></div></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1092409410" data-aue-type="component" data-aue-model="linkfield" data-aue-label="Link Field">
+<div><p class="button-container"><a href="/content/genting-singapore/index/about-us.html" title="About Us Page" data-aue-prop="linkText" data-aue-label="Text" data-aue-type="text" class="button">/content/genting-singapore/index/about-us</a></p></div>
+<div><p data-aue-prop="linkTarget" data-aue-label="Link Target" data-aue-type="text">_blank</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div></div>
+<div></div>
+<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>S$10B</p></div></div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">In market cap</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_351800267" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div></div>
+<div></div>
+<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>1st</p></div></div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">of the only 2 integrated resorts in Singapore</p></div>
+</div>
+<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1811588751" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+<div></div>
+<div></div>
+<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>10</p><p>INDEXES</p></div>
+</div>
+<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">We are a constituent of multiple indexes within the Singapore and global markets</p></div>
+</div>
+ 
+</div></div>`
   // Add other block mock content
 };
 
