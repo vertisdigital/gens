ee00e1628c572fe549743636a1c9306bc7988a39
sudhir.madaan <sudhir.madaan@ogilvy.com>
2025-01-30 17:49:52 +0530

    changes done for linkfield

--- blocks/feature/feature.js
+++ blocks/feature/feature.js
@@ -107,7 +107,6 @@ export default function decorate(block) {
     // Assemble link structure
     linkContainer.appendChild(linkTextDiv);
     linkContainer.appendChild(linkTargetDiv);
-    subHeadingElement.after(linkContainer);
     // aboutUsLeftContent.appendChild(linkContainer);
   }
 
