2145df480d7608a79f80993c6c01daffcb5e3486
Rajat Sharma <rajatsharma.suresh@verticurl.com>
2025-02-07 11:19:32 +0530

    feature-footer fragment.js updated

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -21,9 +21,7 @@ export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
     path = path.replace(/(\.plain)?\.html/, '');
-    newPath = newPath.replace(".html", ".plain.html");
-        const resp = await fetch(`${newPath}`);
-     resp = await fetch(`${path}.plain.html`);
+    const resp = await fetch(`${path}.plain.html`);
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
