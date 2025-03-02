8964c0d1c19a9d3e51fab9cc0141e89df88f2fcb
AvinashRay1702 <avinashkumar.ray@pennywisesolutions.com>
2025-03-02 20:27:55 +0530

    Update hero banner carousel item selector and improve "Read More" link handling

--- blocks/herobanner/herobanner.js
+++ blocks/herobanner/herobanner.js
@@ -114,7 +114,7 @@ export default function decorate(block) {
   }
   heroContainer.appendChild(heroContent);
   const carouselItems = block.querySelectorAll(
-    '[data-aue-model="bannercarousel"],[data-gen-model="tile"]',
+    '[data-aue-model="bannercarousel"],[data-gen-model="featureItem"]',
   );
   const carouselContainer = document.createElement('div');
   carouselContainer.className = 'hero-banner-carousal';
@@ -264,20 +264,30 @@ export default function decorate(block) {
 
     // Extract and append the "Read More" label
     const readMoreLabelElement = itemDivs[3].querySelector('p');
-    if (readMoreLabelElement) {
+    // if (readMoreLabelElement) {
+    //   const readMoreLabelText = readMoreLabelElement.textContent;
+    //   const buttonContainer = readMoreLabelElement.parentElement.nextElementSibling.querySelector('a');
+    //   const href = buttonContainer ? buttonContainer.getAttribute('href') : '';
+    //   const currentUrl = window.location.href;
+    //   const newUrl = currentUrl.replace(window.location.pathname, href);
+    //   const readMoreLabelHtml = `<a class="news-link" href="${newUrl}" target="_blank">${readMoreLabelText}</a>`;
+    //   const readMoreContainer = document.createElement('div');
+    //   moveInstrumentation(itemDivs[3], readMoreContainer);
+    //   readMoreContainer.insertAdjacentHTML('beforeend', readMoreLabelHtml);
+    //   newsLinkDiv.appendChild(readMoreContainer);
+    //   readMoreLabelElement.remove();
+    // }
+   if (readMoreLabelElement) {
       const readMoreLabelText = readMoreLabelElement.textContent;
-      const buttonContainer = readMoreLabelElement.parentElement.nextElementSibling.querySelector('a');
-      const href = buttonContainer ? buttonContainer.getAttribute('href') : '';
-      const currentUrl = window.location.href;
-      const newUrl = currentUrl.replace(window.location.pathname, href);
-      const readMoreLabelHtml = `<a class="news-link" href="${newUrl}" target="_blank">${readMoreLabelText}</a>`;
+      const buttonContainer = itemDivs[4]?.querySelector('a');
+      const href = buttonContainer?.getAttribute('href') ?? '/' ;
+      const readMoreLabelHtml = `<a class="news-link" href="${href}">${readMoreLabelText}</a>`;
       const readMoreContainer = document.createElement('div');
       moveInstrumentation(itemDivs[3], readMoreContainer);
       readMoreContainer.insertAdjacentHTML('beforeend', readMoreLabelHtml);
       newsLinkDiv.appendChild(readMoreContainer);
       readMoreLabelElement.remove();
     }
-
     // Extract the two SVG icons and append them using ImageComponent
     const firstIconLink = block.querySelector(
       'a[href="material-symbols_chevron-left%20(1).svg"]',
