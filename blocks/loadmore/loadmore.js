227c6e2fa22e9fdc05771604a4ba2230d3163339
Swajayakumar Budde <swajay.budde@pennywisesolutions.com>
2025-03-07 19:52:31 +0530

    Fixed the issue with minus values

--- blocks/loadmore/loadmore.js
+++ blocks/loadmore/loadmore.js
@@ -1,42 +1,47 @@
 export default function decorate(block) {
   const [loadmoreEle, itemCount] = block.children;
-  let maxItems = Number.parseInt(itemCount.textContent.trim() || '8', 10);
-  const loadmoreText = loadmoreEle.textContent.trim() ?? 'Load More';
+  let maxItems = Number.parseInt(itemCount?.textContent.trim() || "8", 10);
+  const loadmoreText = loadmoreEle?.textContent.trim() || "Load More";
+
   if (Number.isNaN(maxItems)) {
     maxItems = 8;
   }
-  let activeItems = maxItems;
+  if (maxItems > 0) {
+    let activeItems = maxItems;
 
-  const items = document.querySelectorAll('.historymilestones-wrapper');
-  items.forEach((item, index) => {
-    if (index >= activeItems) {
-      item.style.display = 'none';
-    }
-  });
-
-  // createa a button element
-  const loadmoreButton = document.createElement('button');
-  loadmoreButton.className = 'loadmore-button';
-  loadmoreButton.textContent = `${loadmoreText} (${items.length - activeItems})`;
-  loadmoreEle.innerHTML = '';
-  loadmoreEle.appendChild(loadmoreButton);
-
-  loadmoreButton.addEventListener('click', () => {
-    const newCount = activeItems + maxItems;
-    if (items.length <= newCount) {
-      activeItems = items.length;
-      loadmoreButton.style.display = 'none';
-    } else {
-      activeItems = newCount;
-      loadmoreButton.textContent = `${loadmoreText} (${items.length - activeItems})`;
-    }
+    const items = block.closest(".section").querySelectorAll(".historymilestones-wrapper");
     items.forEach((item, index) => {
-      if (index < activeItems) {
-        item.style.display = 'block';
+      if (index >= activeItems) {
+      item.style.display = 'none';
       }
     });
-  });
 
-  block.innerHTML = '';
-  block.appendChild(loadmoreButton);
+    // createa a button element
+    const loadmoreButton = document.createElement('button');
+    loadmoreButton.className = 'loadmore-button';
+    loadmoreButton.textContent = `${loadmoreText} (${items.length - activeItems})`;
+    loadmoreEle.innerHTML = '';
+    loadmoreEle.appendChild(loadmoreButton);
+
+    loadmoreButton.addEventListener('click', () => {
+      const newCount = activeItems + maxItems;
+      if (items.length <= newCount) {
+        activeItems = items.length;
+        loadmoreButton.style.display = 'none';
+      } else {
+        activeItems = newCount;
+        loadmoreButton.textContent = `${loadmoreText} (${items.length - activeItems})`;
+      }
+      items.forEach((item, index) => {
+        if (index < activeItems) {
+          item.style.display = 'block';
+        }
+      });
+    });
+
+    block.innerHTML = '';
+    block.appendChild(loadmoreButton);
+  } else {
+    block.style.display = "none";
+  }
 }
