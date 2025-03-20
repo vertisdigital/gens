a53c5d50e3bca8d8c030224b57d796715cb6023e
manishverticurl <manish.thakur@verticurl.com>
2025-03-20 13:02:44 +0530

    redirected the routing in utilitys

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,7 @@ import {
   loadCSS,
 } from './aem.js';
 import processTabs from './autoblocks.js';
+import { redirectRouter } from '../shared-components/Utility.js';
 
 
 /**
@@ -81,23 +82,7 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main, isExecute) {
-  let pathName = window.location.href.split('#!/en')
-  pathName = pathName[pathName.length - 1]
-  
-  const redirectableRoutes = {
-    "/company/governance/code-of-conduct": "/index/sustainability/corporate-policies",
-    "/company/governance/human-rights-policy": "/index/sustainability/corporate-policies",
-    "/company/governance/tax-governance-policy": "/index/sustainability/corporate-policies",
-    "/company/governance/whistleblowing-policy": "/index/sustainability/corporate-policies",
-    "/sustainability/sustainability-new/ourapproach": "/index/sustainability",
-    "/sustainability/sustainability-new/report": "/index/sustainability/sustainability-reports",
-    "/investors/publications/annual-report": "/index/investors-overview/publications"
-  }
-  const redirectPathExist=redirectableRoutes[pathName]
-  
-  if(redirectPathExist){
-     window.location.replace(redirectPathExist) 
-  }
+  redirectRouter()
   
   decorateButtons(main);
   decorateIcons(main);
--- shared-components/Utility.js
+++ shared-components/Utility.js
@@ -8,3 +8,24 @@ function stringToHTML(str) {
 }
 
 export default stringToHTML;
+
+
+export function redirectRouter(){
+  let pathName = window.location.href.split('#!/en')
+  pathName = pathName[pathName.length - 1]
+  
+  const redirectableRoutes = {
+    "/company/governance/code-of-conduct": "/index/sustainability/corporate-policies",
+    "/company/governance/human-rights-policy": "/index/sustainability/corporate-policies",
+    "/company/governance/tax-governance-policy": "/index/sustainability/corporate-policies",
+    "/company/governance/whistleblowing-policy": "/index/sustainability/corporate-policies",
+    "/sustainability/sustainability-new/ourapproach": "/index/sustainability",
+    "/sustainability/sustainability-new/report": "/index/sustainability/sustainability-reports",
+    "/investors/publications/annual-report": "/index/investors-overview/publications"
+  }
+  const redirectPathExist=redirectableRoutes[pathName]
+  
+  if(redirectPathExist){
+     window.location.replace(redirectPathExist) 
+  }
+}
