0bcb6a4e6f8727508256e5d823e7829416f1c77e
Nikhil Seth <nikhil.seth@verticurl.com>
2025-04-21 15:48:44 +0530

    Separator styles added

--- blocks/separator/separator.js
+++ blocks/separator/separator.js
@@ -1,1 +1,10 @@
-// add js for separator
+export default function decorate(block) {
+  block.classList.add('container');
+  let separatorLine = block.querySelector('.separator-line');
+  if (!separatorLine) {
+    separatorLine = document.createElement('div');
+    separatorLine.className = 'separator-line container';
+  }
+  block.textContent = '';
+  block.appendChild(separatorLine);
+}
