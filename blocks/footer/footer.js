4bb808583dacad4506afa7d4b2919f36089f5826
sudhir <sudhir.madaan@ogilvy.com>
2025-07-03 12:48:46 +0530

    Update console log timestamp in footer.js for enhanced tracking precision

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -26,7 +26,7 @@ const handleAccordionToggle = (e, keyboardTrigger = false) => {
  */
 export default async function decorate(block) {
   // load footer as fragment
-  console.log('footer-03July-12:42pm');
+  console.log('footer-03July-12:48pm');
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
