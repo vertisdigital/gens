98050508ed1096bcaadc8fa8f5c9adab5c1286c2
huylam <huy@vertis.digital>
2026-02-15 21:54:42 +0700

    adobe launch link

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,6 +2,6 @@
 
 // Create and append Adobe Launch script
 const adobeLaunchScript = document.createElement('script');
-adobeLaunchScript.src = 'https://assets.adobedtm.com/9a26c7c29956/9d78c9fea2b9/launch-c830de7b55b7-development.min.js';
+adobeLaunchScript.src = 'https://assets.adobedtm.com/9a26c7c29956/9d78c9fea2b9/launch-82eb0a5e0018.min.js';
 adobeLaunchScript.async = true;
 document.head.appendChild(adobeLaunchScript);
