11af89b219d4ed35c82a224fbc175dd086d9d6e5
huylam <huy@vertis.digital>
2026-03-05 21:44:07 +0700

    adobe script for staging

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,6 +2,6 @@
 
 // Create and append Adobe Launch script
 const adobeLaunchScript = document.createElement('script');
-adobeLaunchScript.src = 'https://assets.adobedtm.com/9a26c7c29956/9d78c9fea2b9/launch-82eb0a5e0018.min.js';
+adobeLaunchScript.src = 'https://assets.adobedtm.com/9a26c7c29956/9d78c9fea2b9/launch-0643cd1b44f3-staging.min.js';
 adobeLaunchScript.async = true;
 document.head.appendChild(adobeLaunchScript);
